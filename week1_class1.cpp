fb634998597c8400037bcec472fa605aa138bbd8
sathishkumarg5024 <sathishkumarg5024@gmail.com>
2023-02-24 23:47:39 +0530

    minor changes

---
+++ week1_class1.cpp
@@ -0,0 +1,9 @@
+// Approach to solve a problem
+
+/*
+1. Understand the problem.
+2. Think of the solution
+3. Express the solution in the form of flow chart or pseudo code.
+4. Verify all the test cases.
+5. Execute the solution using a programming language.
+*/
